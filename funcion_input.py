af42f9cb7a58644e0e6350a39336561193d2bfa1
ccencho <nccencho@gmail.com>
2020-03-28 18:43:04 -0500

    Primeros codigos

---
+++ funcion_input.py
@@ -0,0 +1,14 @@
+a=input("Ingresa un número")
+#print("Mi nombre es:"+a)
+
+'''
+a=float(input("Ingrese el primer número: "))
+b=float(input("Ingrese el segundo número: "))
+c=a+b
+print(f"La suma es: {c}")
+'''
+a=int(input("Ingrese el primer número: "))
+b=int(input(f"Ingresa un número diferente de {a}: "))
+print(f"Los números ingresados son {a} y {b}")
+
+
---
+++ funcion_print.py
@@ -0,0 +1,9 @@
+print('Hola Mundo')
+print("Hola Mundo")
+
+a=10
+print(a)
+print('El valor de la variable es:',a)
+print('El valor de la variable es: {}'.format(a))
+print(f'El valor de la variable es: {a}')
+print('El valor de la variable es: '+ str(a))
