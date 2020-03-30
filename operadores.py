9c35df26fa6e2f4858adbf734262cb46daf87bbf
ccencho <nccencho@gmail.com>
2020-03-29 21:07:17 -0500

    Codigo Operadores

---
+++ operadores.py
@@ -0,0 +1,22 @@
+a=5
+b=2
+suma=a+b 
+resta=a-b 
+multiplicacion=a*b 
+division=a/b
+division_int=a//b
+potencia=a**b
+modulo=a%b 
+print("La suma es: ",suma)
+print("La resta es: ",resta)
+print("La multiplicación: ",multiplicacion)
+print("La división: ",division)
+print("La división entera es: ",division_int)
+print("La potencia es: ",potencia)
+print("El modulo(residio)es: ",modulo)
+
+#Operación con flotantes
+a=5.2
+b=3.5
+
+print("La suma de 2 números flotantes es:",a*b)
